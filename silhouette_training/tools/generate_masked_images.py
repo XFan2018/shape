65c76ad42ec868ff4eafaf12a4435c3c34450677
Xingye <xingyecoin@gmail.com>
2021-06-06 19:18:34 -0400

    Add: bagnet mask generator

--- silhouette_training/tools/generate_masked_images.py
+++ silhouette_training/tools/generate_masked_images.py
@@ -1,108 +1,269 @@
-import PIL
-import numpy as np
-from PIL import Image
-import torchvision
+import logging
+from pathlib import Path
+from typing import Any, List
+
 import torch
+import torchvision
+from PIL import Image
+from bagnets import pytorchnet
+from torchvision import transforms
 
-file_path = "/Users/leo/PycharmProjects/summerProject/project2/fish.JPEG"
-img = Image.open(file_path)
+delimiter = "\\"
+file_path = r"D:\projects\shape_dataset\imagenet_val\n01440764\ILSVRC2012_val_00007197.JPEG"
+save_path = r"C:\Users\x44fa\Dropbox\York_summer_project\occluded_images"
+logging.basicConfig(format="%(asctime)s — logger %(name)s — %(levelname)s — %(message)s",
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+extension = "png"
 
 
-def mask_patch(img, patch_size):
-    """
-    :param img: image
-    :return:
-             patch size = 20
-             a list of 25 + 1 + 1 = 27 images will be returned (type = <class 'PIL.Image.Image'>)
-             1st "1" is the image masked with all patches simultaneously, 2nd "1" is the original image
-    """
-    patch_size_list = [20, 30, 40, 50, 60, 70]
-    if patch_size not in patch_size_list:
-        raise Exception("Sorry, no patch size must be one of 20, 30, 40, 50, 60, 70 numbers")
-    switcher = {
-        20: {
-            "offset": 22,
-            "interval": 20 * 2,
-            "img_number": 26
-        },
+class MaskImageGenerator:
+    def __init__(self, dataset: str, dst: str, model: Any):
+        self.dataset = dataset
+        self.dst = dst
+        self.switcher = {
+            20: {
+                "offset": 22,
+                "interval": 20 * 2,
+                "img_number": 26
+            },
 
-        30: {
-            "offset": 7,
-            "interval": 30 * 2,
-            "img_number": 17
-        },
+            30: {
+                "offset": 7,
+                "interval": 30 * 2,
+                "img_number": 17
+            },
 
-        40: {
-            "offset": 12,
-            "interval": 40 * 2,
-            "img_number": 10
-        },
+            40: {
+                "offset": 12,
+                "interval": 40 * 2,
+                "img_number": 10
+            },
 
-        50: {
-            "offset": 37,
-            "interval": 50 * 2,
-            "img_number": 5
-        },
+            50: {
+                "offset": 37,
+                "interval": 50 * 2,
+                "img_number": 5
+            },
 
-        60: {
-            "offset": 22,
-            "interval": 60 * 2,
-            "img_number": 5
-        },
+            60: {
+                "offset": 22,
+                "interval": 60 * 2,
+                "img_number": 5
+            },
 
-        70: {
-            "offset": 7,
-            "interval": 70 * 2,
-            "img_number": 5
+            70: {
+                "offset": 7,
+                "interval": 70 * 2,
+                "img_number": 5
+            }
         }
+        self.isolation = "isolation"
+        self.combination = "combination"
+        self.model = model
+        self.data_transforms = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+        self.org_data_transforms = transforms.Compose([
+            # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            transforms.Resize((224, 224)),
+            transforms.ToTensor()
+        ])
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    }
-    list_img = []  # a list of 27 images, see return for details
-    list_img_pixel_map = []  # 27 pixel maps for the 27 images
-    img = img.resize((224, 224))  # original image is re-sized to 224
-    pixel_map = img.load()
-    for _ in range(switcher[patch_size]["img_number"]):
-        # create n images and pixel maps from original images
-        new_img = Image.new(img.mode, img.size)
-        pixel_map_new = new_img.load()
+    def mask_patch(self, img: Image.Image, patch_size: int):
+        """
+        :return:
+                 patch size = 20
+                 a list of 25 + 1 + 1 = 27 images will be returned (type = <class 'PIL.Image.Image'>)
+                 1st "1" is the image masked with all patches simultaneously, 2nd "1" is the original image
+        """
+        patch_size_list = [20, 30, 40, 50, 60, 70]
+        if patch_size not in patch_size_list:
+            raise Exception("patch size must be one of 20, 30, 40, 50, 60, 70 numbers")
 
-        # assign pixel values so that they are all identical to the original images
-        for i in range(new_img.size[0]):
-            for j in range(new_img.size[1]):
-                pixel_map_new[i, j] = pixel_map[i, j]
+        list_img = []  # a list of 27 images, see return for details
+        list_img_pixel_map = []  # 27 pixel maps for the 27 images
+        img = img.resize((224, 224))  # original image is re-sized to 224
+        pixel_map = img.load()
+        for _ in range(self.switcher[patch_size]["img_number"]):
+            # create n images and pixel maps from original images
+            new_img = Image.new(img.mode, img.size)
+            pixel_map_new = new_img.load()
 
-        # append them to the list
-        list_img.append(new_img)
-        list_img_pixel_map.append(pixel_map_new)
+            # assign pixel values so that they are all identical to the original images
+            for i in range(new_img.size[0]):
+                for j in range(new_img.size[1]):
+                    pixel_map_new[i, j] = pixel_map[i, j]
 
-    # create iterator for the two lists (images and pixel maps)
-    iter_img_pixel_map = iter(list_img_pixel_map)
+            # append them to the list
+            list_img.append(new_img)
+            list_img_pixel_map.append(pixel_map_new)
 
-    # generate the masks location
-    list_offset_row = range(switcher[patch_size]["offset"], 224 - switcher[patch_size]["offset"],
-                            switcher[patch_size]["interval"])
-    list_offset_col = range(switcher[patch_size]["offset"], 224 - switcher[patch_size]["offset"],
-                            switcher[patch_size]["interval"])
-    for i in list_offset_col:
-        print(i)
-    for offset_row in list_offset_row:
-        for offset_col in list_offset_col:
+        # create iterator for the two lists (images and pixel maps)
+        iter_img_pixel_map = iter(list_img_pixel_map)
 
-            # image masked with individual patch
-            pixel_map_individual_patch = next(iter_img_pixel_map)
+        # generate the masks location
+        list_offset_row = range(self.switcher[patch_size]["offset"], 224 - self.switcher[patch_size]["offset"],
+                                self.switcher[patch_size]["interval"])
+        list_offset_col = range(self.switcher[patch_size]["offset"], 224 - self.switcher[patch_size]["offset"],
+                                self.switcher[patch_size]["interval"])
+        for offset_row in list_offset_row:
+            for offset_col in list_offset_col:
 
-            # image masked with all patches simultaneously
-            pixel_map_all_patch = list_img_pixel_map[switcher[patch_size]["img_number"] - 1]
-            for i in range(offset_row, offset_row + patch_size):
-                for j in range(offset_col, offset_col + patch_size):
-                    pixel_map_individual_patch[i, j] = (0, 0, 0)
-                    pixel_map_all_patch[i, j] = (0, 0, 0)
+                # image masked with individual patch
+                pixel_map_individual_patch = next(iter_img_pixel_map)
 
-    # add original image to the end of the list
-    list_img.append(img)
-    return list_img
+                # image masked with all patches simultaneously
+                pixel_map_all_patch = list_img_pixel_map[self.switcher[patch_size]["img_number"] - 1]
+                for i in range(offset_row, offset_row + patch_size):
+                    for j in range(offset_col, offset_col + patch_size):
+                        try:
+                            pixel_map_individual_patch[i, j] = (0, 0, 0)
+                            pixel_map_all_patch[i, j] = (0, 0, 0)
+                        except TypeError:
+                            pixel_map_individual_patch = (0,)
+                            pixel_map_all_patch = (0,)
+
+        # add original image to the end of the list
+        list_img.append(img)
+        return list_img
+
+    # generate two occluded datasets for left side and right side of the eq, repectively
+    # Isolation dataset contains many images with an individual patch for one data
+    # Combination dataset contains one image with all patches for one data
+    def make_dataset(self, patch_size: int):
+        self.copy_dataset_folders(patch_size)
+        for idx, img_class_path in enumerate(Path.iterdir(Path(self.dataset))):
+            image_class = str(img_class_path).split(delimiter)[-1]
+            logger.info(image_class)
+            for img_path in Path.iterdir(Path(self.dataset).joinpath(image_class)):
+                image_name = str(img_path).split(delimiter)[-1]
+                image_name = image_name.replace(".JPEG", "")
+                print(idx)
+                logger.info(image_name)
+                logger.info(img_path)
+                img = Image.open(img_path)
+                *isolation_images, combination_image, original_image = self.mask_patch(img, patch_size)
+                # save combination image
+                combination_image.save(Path(self.dst).
+                                       joinpath(f"{self.combination}_{patch_size}").
+                                       joinpath(image_class).
+                                       joinpath(image_name + f".{extension}"), format=extension)
+                # save isolation images
+                for i, isolation_image in enumerate(isolation_images):
+                    isolation_image.save(Path(self.dst).
+                                         joinpath(f"{self.isolation}_{patch_size}").
+                                         joinpath(image_class).
+                                         joinpath(f"{image_name}_{i}.{extension}"), format=extension)
+
+    # copy class folders of the dataset to another path
+    def copy_dataset_folders(self, patch_size: int):
+        for d in Path.iterdir(Path(self.dataset)):
+            class_dir = str(d).split(delimiter)[-1]
+            if class_dir.startswith("."):
+                continue
+            logger.info(class_dir)
+            try:
+                Path(self.dst).joinpath(f"{self.isolation}_{patch_size}").joinpath(class_dir).mkdir(mode=644,
+                                                                                                    parents=True)
+                Path(self.dst).joinpath(f"{self.combination}_{patch_size}").joinpath(class_dir).mkdir(mode=644,
+                                                                                                      parents=True)
+            except FileExistsError:
+                logger.info("file already exists")
+
+    # calculate LHS: mean logits of images that are manipulated by an individual patch
+    def calculate_lhs(self, patch_size: int) -> List:
+        result = []
+        dataset = Path(self.dst).joinpath(f"{self.isolation}_{patch_size}")
+        data = torchvision.datasets.ImageFolder(dataset, self.data_transforms)
+        batch_size = self.switcher[patch_size]["img_number"] - 1
+        dataloader = torch.utils.data.DataLoader(data,
+                                                 batch_size=batch_size,
+                                                 shuffle=False,
+                                                 num_workers=2)
+        self.model = self.model.to(self.device)
+        self.model.eval()
+        for i, (image, label) in enumerate(dataloader):
+            image = image.to(self.device)
+            label = label.to(self.device)
+            with torch.set_grad_enabled(False):
+                outputs = self.model(image)
+                val = outputs[torch.arange(4), label]
+                mean_val = torch.mean(val)
+                result.append(mean_val.item())
+                logger.info(f"iter: {i}, mean_val: {mean_val}, label: {label}")
+        return result
+
+    # calculate RHS: logits of the images that are manipulated by all patches at once
+    def calculate_rhs(self, patch_size: int) -> List:
+        result = []
+        dataset = Path(self.dst).joinpath(f"{self.combination}_{patch_size}")
+        data = torchvision.datasets.ImageFolder(dataset, self.data_transforms)
+        dataloader = torch.utils.data.DataLoader(data,
+                                                 shuffle=False,  # batch size default is 1
+                                                 num_workers=2)
+        self.model = self.model.to(self.device)
+        self.model.eval()
+        for i, (image, label) in enumerate(dataloader):
+            image = image.to(self.device)
+            label = label.to(self.device)
+            with torch.set_grad_enabled(False):
+                outputs = self.model(image)
+                val = outputs[torch.arange(1), label]
+                val = torch.squeeze(val)
+                result.append(val.item())
+                logger.info(f"iter: {i}, mean_val: {val}, label: {label}")
+        return result
+
+    def calculate_org_logit(self) -> List:
+        result = []
+        data = torchvision.datasets.ImageFolder(self.dataset, self.org_data_transforms)
+        dataloader = torch.utils.data.DataLoader(data,
+                                                 shuffle=False,  # batch size default is 1
+                                                 num_workers=2)
+        self.model = self.model.to(self.device)
+        self.model.eval()
+        for i, (image, label) in enumerate(dataloader):
+            image = image.to(self.device)
+            label = label.to(self.device)
+            with torch.set_grad_enabled(False):
+                outputs = self.model(image)
+                val = outputs[torch.arange(1), label]
+                val = torch.squeeze(val)
+                result.append(val.item())
+                logger.info(f"iter: {i}, mean_val: {val}, label: {label}")
+        return result
+
+    def pickle_data(self, data: List, file_name: str):
+        import pickle
+        with open(file_name, 'wb') as f:
+            pickle.dump(data, f)
+
+    def load_data(self, file_name: str) -> List:
+        import pickle
+        with open(file_name, 'rb') as f:
+            return pickle.load(f)
 
 
-result = mask_patch(img, 20)
-for im in result:
-    im.show()
+if __name__ == "__main__":
+    import numpy as np
+    model = pytorchnet.bagnet33(pretrained=True)
+    generator = MaskImageGenerator(r"D:\projects\shape_dataset\imagenet_val_testing_dataset_Copy",
+                                   r"C:\Users\x44fa\Dropbox\York_summer_project\occluded_images",
+                                   model)
+    org = np.array(generator.load_data("org.pkl"))
+    all_pat = np.array(generator.load_data("rhs.pkl"))
+    single_pat = np.array(generator.load_data("lhs.pkl"))
+
+    LHS = single_pat - org
+    RHS = all_pat - org
+
+    np.corrcoef(LHS, RHS)
+
+
+
+
